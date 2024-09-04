907a6717346e3adb23b297274d9f15c145f8bd55
Bryan Zacarias <Bryan_Zacarias@outlook.com>
2024-09-04 15:16:14 -0400

    Quick Fix for null div element

--- js/datalayer.js
+++ js/datalayer.js
@@ -7,16 +7,19 @@ document.getElementById('product-component-1724121027841')
 document.getElementById('product-component-1724121584057')
     .addEventListener('click', sendProductInfo('8646623592741', 'you are naan-stoppable! happy birthday!', '7.99'));
 
-document.getElementById('product-component-1724121810286')
-    .addEventListener('click', sendProductInfo('9561411092773', 'fwords bundle', '21.99'));
-
-document.getElementById('desk-product-component-1724121810286')
-    .addEventListener('click', sendProductInfo('9561411092773', 'fwords bundle', '21.99'));    
+if (window.innerWidth < 768) {
+    document.getElementById('product-component-1724121810286')
+        .addEventListener('click', sendProductInfo('9561411092773', 'fwords bundle', '21.99'));    
+}
+else {
+    document.getElementById('desk-product-component-1724121810286')
+        .addEventListener('click', sendProductInfo('9561411092773', 'fwords bundle', '21.99'));
+}
 
 function sendProductInfo(id, name, amount) {
     console.log("data layer being sent");
-    gtag("event", "add_to_cart", {
-        currency: "USD",
+    gtag('event', 'add_to_cart', {
+        currency: 'USD',
         value: amount,
         items: [
             {
