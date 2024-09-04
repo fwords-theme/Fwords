f88807882c8192bc05c2a16120ec631b84b0941a
Bryan Zacarias <Bryan_Zacarias@outlook.com>
2024-09-04 16:14:47 -0400

    Moved data layer push code to index.js

--- js/index.js
+++ js/index.js
@@ -159,3 +159,17 @@ function sendId(n) {
     const productID = parseInt(n);
     localStorage.setItem("product_id", productID);
 }
+
+function sendProductInfo(id, name, amount) {
+    console.log("data layer being sent");
+    gtag('event', 'add_to_cart', {
+        currency: 'USD',
+        value: amount,
+        items: [
+            {
+                item_id: id,
+                item_name: name
+            }
+        ]
+    });
+}
