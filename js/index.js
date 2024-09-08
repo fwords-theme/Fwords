75ff0961467579163f59442be273f1ce18079807
Bryan Zacarias <Bryan_Zacarias@outlook.com>
2024-09-08 13:56:38 -0400

    Changed to event listener instead of onclick for buy button

--- js/index.js
+++ js/index.js
@@ -12,18 +12,3 @@ function sendId(n) {
     const productID = parseInt(n);
     localStorage.setItem("product_id", productID);
 }
-
-// Google Analytics event tracking for adding product to cart
-function sendProductInfo(productId, productName, productPrice) {
-    gtag("event", "add_to_cart", {
-        currency: "USD",
-        value: productPrice,
-        items: [
-            {
-                item_id: productId,
-                item_name: productName,
-                price: productPrice,
-            }
-        ]
-    });
-}
