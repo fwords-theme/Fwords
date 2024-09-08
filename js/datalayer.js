cae99d95ec255cd3a3b767cf47ced223e21b67ae
Bryan Zacarias <Bryan_Zacarias@outlook.com>
2024-09-08 12:00:08 -0400

    Split js code for preloading and postloading

--- js/datalayer.js
+++ js/datalayer.js
@@ -1,13 +1,1 @@
-function sendProductInfo(productId, productName, productPrice) {
-    gtag("event", "add_to_cart", {
-        currency: "USD",
-        value: productPrice,
-        items: [
-            {
-                item_id: productId,
-                item_name: productName,
-                price: productPrice,
-            }
-        ]
-    });
-}
+
