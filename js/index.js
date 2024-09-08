6a37c06df5c38297176fa582c33cec842de3417b
Bryan Zacarias <Bryan_Zacarias@outlook.com>
2024-09-08 15:29:38 -0400

    Moved gtag code to index.js and load at the end

--- js/index.js
+++ js/index.js
@@ -12,3 +12,18 @@ function sendId(n) {
     const productID = parseInt(n);
     localStorage.setItem("product_id", productID);
 }
+
+document.getElementById('product-component-1724120776527').addEventListener('click', function() {
+    console.log("gtag event fired");
+    gtag("event", "add_to_cart", {
+        currency: "USD",
+        value: 7.99,
+        items: [
+            {
+                item_id: "8646557303077",
+                item_name: "i love you so much my daal-ling",
+                price: 7.99
+            }
+        ]
+    })
+});
